811f3e9ceb7f092e1c3a3de0eccb1a726c52e3ef
Jolyon Bloomfield <jkb84@cornell.edu>
2019-01-16 23:43:08 -0500

    Adding Hartree on+off sweep capabilities

--- run_sweep.py
+++ run_sweep.py
@@ -15,30 +15,31 @@ from evolver.model import Model
 
 # Initialize all settings
 lamda = 1e-9
-filename = "data/large_Hoff_sweep"
-hartree = True
-if hartree:
-    num_modes = 40
-else:
-    num_modes = 2
+hartree_settings = [True, False]  # Make a list of Hartree settings here
+filename = "data/large_sweep"
 
 # Background fields
 # Note that a step of 1 only does the start value
 phi0start = 25
 phi0stop = 32
-phi0steps = 1
-phi0dotstart = -0.1
-phi0dotstop = 0.1
-phi0dotsteps = 1
-# Number of runs to perform at each step
-numruns = 20
+phi0steps = 3
+phi0dotstart = -0.05
+phi0dotstop = 0.05
+phi0dotsteps = 3
+
+# Number of runs to perform at each step (for Hartree on)
+numruns = 2
+
+# Fix the number of modes
+if True in hartree_settings:
+    num_modes = 40
+else:
+    num_modes = 2
 
 # Construct our steps
 phi0s = np.linspace(phi0start, phi0stop, phi0steps)
 phi0dots = np.linspace(phi0dotstart, phi0dotstop, phi0dotsteps)
-if not hartree:
-    numruns = 1
-runnums = [i + 1 for i in range(numruns)]
+
 
 run = 0
 start = time.time()
@@ -52,27 +53,22 @@ package = create_package(phi0=None,
                          infmodel=LambdaPhi4(lamda=lamda),
                          end_time=5000*sqrt(1e-6/lamda),
                          basefilename=None,
-                         hartree=hartree,
                          perturbBD=True,
                          timestepinfo=[200, 10],
                          num_k_modes=num_modes)
 
-for x, y, runnum in tqdm(list(itertools.product(phi0s, phi0dots, runnums))):
-    # Construct the filename
-    run += 1
-    fn = filename + "-{}".format(run)
-    infofile.write("{}\t{}\t{}\n".format(fn, x, y))
-
+def perform_run(phi0, phi0dot, filename, hartree):
     # Update package
-    package['phi0'] = x
-    package['phi0dot'] = y
-    package['basefilename'] = fn
+    package['phi0'] = phi0
+    package['phi0dot'] = phi0dot
+    package['basefilename'] = filename
+    package['hartree'] = hartree
 
     parameters = create_parameters(package)
 
     # Create the model
     model = Model(parameters)
-    model.save(fn + ".params")
+    model.save(filename + ".params")
 
     # Construct the driver
     driver = Driver(model)
@@ -87,6 +83,22 @@ for x, y, runnum in tqdm(list(itertools.product(phi0s, phi0dots, runnums))):
     elif driver.status == Status.Terminated:
         with open(fn + ".info", "a") as f:
             f.write("Evolution completed with message: {}".format(driver.error_msg))
+
+# Sweep through all the runs
+for x, y in tqdm(list(itertools.product(phi0s, phi0dots))):
+    run += 1
+    fn = filename + "-{}".format(run)
+    if False in hartree_settings:
+        # Construct the filename
+        fnoff = fn + "-off"
+        infofile.write("{}\t{}\t{}\n".format(fnoff, x, y))
+        perform_run(x, y, fnoff, False)
+    if True in hartree_settings:
+        for i in range(numruns):
+            # Construct the filename
+            fnon = fn + "-" + str(i + 1)
+            infofile.write("{}\t{}\t{}\n".format(fnon, x, y))
+            perform_run(x, y, fnon, True)
 
 infofile.close()
 
